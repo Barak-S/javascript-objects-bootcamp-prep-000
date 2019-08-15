e1065bef6a077403d13115125983ecde918b2c40
Barak Saidoff <barak.saidoff@gmail.com>
2019-08-15 22:54:08 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,9 +1,9 @@
 var playlist={artistName:”song”};
 
 function updatePlaylist(playlist,artistName,songTitle){
-playlist[artistName]=songTitle
+  return playlist[artistName]=songTitle
 }
 
 function removeFromPlaylist(playlist, artistName){
-delete playlist[artistName];
+  delete playlist[artistName];
 }
